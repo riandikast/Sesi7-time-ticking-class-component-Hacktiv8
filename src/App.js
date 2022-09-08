780cc62b3fa95c19b486c476faab564162c2bf5c
Riandika <riandikasp@gmail.com>
2022-09-08 21:03:18 +0700

    class-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Realtime Clock</h1>
+        <hr></hr>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID)
+  }
+  
 }
 
 export default App;
